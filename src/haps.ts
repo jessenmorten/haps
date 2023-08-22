ed196e0c76e46cf90647e9de05764bc9fac6441d
Morten Jessen <mortenjessen5d@gmail.com>
2023-08-22 20:57:18 +0200

    perf: only init on hotkeys pressed

--- src/haps.ts
+++ src/haps.ts
@@ -1,47 +1,53 @@
-(function () {
-    const w: Window &
-        typeof globalThis & { hapsIntervalId: number | undefined } =
-        window as any;
+console.info("Haps injected");
 
-    if (w.hapsIntervalId) {
-        clearInterval(w.hapsIntervalId);
-        return;
-    }
+function HapsInit() {
+    const elements = [...document.querySelectorAll("a")];
 
-    w.hapsIntervalId = setInterval(() => {
-        const elements = [...document.querySelectorAll("a")];
+    for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        const content = element.textContent || "";
+        const skip =
+            element.classList.contains("hapsable") || content.trim() === "";
 
-        for (let i = 0; i < elements.length; i++) {
-            const element = elements[i];
-            const content = element.textContent || "";
-            const skip =
-                element.classList.contains("hapsable") || content.trim() === "";
-
-            if (skip) {
-                continue;
-            }
-
-            element.classList.add("hapsable");
-            element.addEventListener("click", (e) => {
-                if (document.body.classList.contains("haps")) {
-                    e.preventDefault();
-                    document.body.classList.remove("haps");
-                    const content = element.textContent || "";
-                    navigator.clipboard.writeText(content.trim());
-                }
-            });
+        if (skip) {
+            continue;
         }
 
-        document.addEventListener("keydown", (e) => {
-            if (e.ctrlKey && e.altKey) {
-                document.body.classList.add("haps");
-            }
-        });
-
-        document.addEventListener("keyup", (e) => {
-            if (!e.ctrlKey || !e.altKey) {
+        element.classList.add("hapsable");
+        element.addEventListener("click", (e) => {
+            if (document.body.classList.contains("haps")) {
+                e.preventDefault();
                 document.body.classList.remove("haps");
+                const content = element.textContent || "";
+                navigator.clipboard.writeText(content.trim());
             }
         });
-    }, 1000);
-})();
+    }
+
+    document.addEventListener("keydown", (e) => {
+        if (e.ctrlKey && e.altKey) {
+            document.body.classList.add("haps");
+        }
+    });
+
+    document.addEventListener("keyup", (e) => {
+        if (!e.ctrlKey || !e.altKey) {
+            document.body.classList.remove("haps");
+        }
+    });
+}
+
+document.addEventListener("keydown", (e) => {
+    const w: Window &
+        typeof globalThis & {
+            hapsInitiated: boolean | undefined;
+        } = window as any;
+
+    if (e.ctrlKey && e.altKey && !e.repeat) {
+        if (!w.hapsInitiated) {
+            w.hapsInitiated = true;
+            HapsInit();
+            console.info("Haps initiated");
+        }
+    }
+});
